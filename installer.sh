e80a4c5a46bf63dc06c1108a0c8aeda1c7ebd129
Gino Prasad <ginoprasad@gmail.com>
2024-11-14 19:06:24 -0800

    Updated installer with default schedules

--- installer.sh
+++ installer.sh
@@ -1,12 +1,17 @@
 #!/usr/bin/env python3
 
 import os, glob
+import subprocess as sp
 
 os.makedirs('cache', exist_ok=True)
 os.makedirs('log', exist_ok=True)
+os.makedirs('schedules', exist_ok=True)
 
 with open('cache/events_cache.py', 'w') as outfile:
 	outfile.write('events = []\n')
+
+for file in ['schedules/gcal_personal.txt', 'schedules/gcal_school.txt', 'schedules/local_events.txt', 'schedules/local_weekly.txt', 'cache/m_cache.txt']:
+	sp.run("touch " + file, shell=True)
 
 if os.environ['SHELL'].strip() == '':
 	exit(1)
