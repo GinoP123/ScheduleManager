82e09139dbc721635579f2cabc4ff2e645373f49
Gino Prasad <ginoprasad@gmail.com>
2024-11-14 18:51:44 -0800

    Updated

---
+++ external_scripts.py
@@ -0,0 +1,3 @@
+open_file_script = "/Users/ginoprasad/Scripts/miscellaneous/open_file.sh"
+open_link_script = "/Users/ginoprasad/Scripts/miscellaneous/open_url.sh"
+schedule_open_url = "/Users/ginoprasad/Scripts/ScheduleManager/schedule_open_url.sh"
