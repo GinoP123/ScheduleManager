dc8f684debec8c51960c1a4311d638190e9df3e9
Gino Prasad <ginoprasad@gmail.com>
2024-11-14 19:01:05 -0800

    Updated Installer

--- installer.sh
+++ installer.sh
@@ -1,4 +1,16 @@
-#!/bin/zsh
+#!/usr/bin/env python3
 
-mkdir -p cache
-mkdir -p log
+import os, glob
+
+os.makedirs('cache', exist_ok=True)
+os.makedirs('log', exist_ok=True)
+
+if os.environ['SHELL'].strip() == '':
+	exit(1)
+
+for file in glob.glob("*.sh") + glob.glob("*/*.sh") + glob.glob("bin/*"):
+	with open(file) as infile:
+		file_output = infile.read().replace('#!/bin/zsh', '#!' + os.environ['SHELL'])
+
+	with open(file, 'w') as outfile:
+		outfile.write(file_output)
