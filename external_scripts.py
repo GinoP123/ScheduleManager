8ed6489bfa77a24653ec1afe3d6355db9cf95ede
Gino Prasad <ginoprasad@gmail.com>
2026-01-10 11:50:51 -0800

    Made paths Home directory agnostic

--- external_scripts.py
+++ external_scripts.py
@@ -1,4 +1,6 @@
+import os
+
 open_file_script = "/usr/bin/subl"
-open_link_script = "/home/ginoprasad/Scripts/miscellaneous/open_url.sh"
-schedule_open_url = "/home/ginoprasad/Scripts/ScheduleManager/schedule_open_url.sh"
+open_link_script = os.path.expanduser("~/Scripts/miscellaneous/open_url.sh")
+schedule_open_url = os.path.expanduser("~/Scripts/ScheduleManager/schedule_open_url.sh")
 shell_path = "/usr/bin/bash"
