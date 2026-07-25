9e80d79ee08a6bf8707f7368098729b2ea371346
Gino Prasad <ginoprasad@gmail.com>
2026-07-25 14:35:38 -0700

    Updated

--- update_chrome_profile_names_mac.sh
+++ update_chrome_profile_names_mac.sh
@@ -3,6 +3,7 @@
 original_name="$1"
 new_name="$2"
 
+chrome_path="/Applications/Google Chrome.app/Contents/MacOS/Google Chrome"
 profile_path="$HOME/Library/Application Support/Google/Chrome"
 
 if [[ -d "$profile_path/$original_name" ]]; then
@@ -16,3 +17,4 @@ rm -f "$profile_path/Local State"
 cd "$profile_path/$new_name"
 LC_ALL=C find . -type f -exec sed -i '' "s/$original_name/$new_name/g" {} +
 
+"$chrome_path" --profile-directory="$new_name"
