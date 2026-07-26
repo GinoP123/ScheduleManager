aa8c69cb182122fa74d88aa934d5b5a12a79a02b
Gino Prasad <ginoprasad@gmail.com>
2026-07-25 23:07:55 -0700

    Fixed path error

--- carry_over_tasks.py
+++ carry_over_tasks.py
@@ -50,17 +50,21 @@ def get_events_day(date):
     return events
 
 
-for user in settings.CHROME_PROFILES:
-	token_file = settings.TOKEN_FILES[user]
-	creds = Credentials.from_authorized_user_file(token_file, settings.SCOPES)
-	service = build('calendar', 'v3', credentials=creds)
 
-	today = (datetime.datetime.now(ZoneInfo(settings.TIMEZONE))).date()
-	tomorrow = today +  datetime.timedelta(days=1)
-	for event_summary in get_events_day(today) - get_events_day(tomorrow):
-	    event_body = get_event_template(event_summary)
-	    created_event = service.events().insert(
-	        calendarId='primary', 
-	        body=event_body
-	    ).execute()
+if __name__ == '__main__':
+    os.chdir(os.path.dirname(sys.argv[0]))
+    os.environ['MAILTO'] = ""
+    for user in settings.CHROME_PROFILES:
+    	token_file = settings.TOKEN_FILES[user]
+    	creds = Credentials.from_authorized_user_file(token_file, settings.SCOPES)
+    	service = build('calendar', 'v3', credentials=creds)
+
+    	today = (datetime.datetime.now(ZoneInfo(settings.TIMEZONE))).date()
+    	tomorrow = today +  datetime.timedelta(days=1)
+    	for event_summary in get_events_day(today) - get_events_day(tomorrow):
+    	    event_body = get_event_template(event_summary)
+    	    created_event = service.events().insert(
+    	        calendarId='primary', 
+    	        body=event_body
+    	    ).execute()
 
